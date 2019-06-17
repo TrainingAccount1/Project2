4b000f1a8c6ee179060a752cd302489be4848380
shivhus <shivhus@LONMW15922.fm.rbsgrp.net>
2019-06-17 15:17:18 +0530

    push

---
+++ StaticEx/src/com/MainApp.java
@@ -0,0 +1,32 @@
+package com;
+
+/*
+class App{
+	
+	static int a=0;
+	
+static void display() {
+	System.out.println("Value "+a);
+}
+}
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		App.display();
+		
+	}
+
+}*/
+
+class Adder{  
+static int add(int a,int b){return a+b;}  
+static int add(int a,int b,int c){return a+b+c;}  
+}  
+class MainApp{  
+public static void main(String[] args){  
+System.out.println(Adder.add(11,11));  
+System.out.println(Adder.add(11,11,11));  
+}} 
